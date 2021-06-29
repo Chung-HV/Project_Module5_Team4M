acf278763a2ca438fec9406adffaf6f2ff86681a
quanle031198 <bluestargaming000@gmail.com>
2021-06-29 18:11:34 +0700

    frontend header footer

--- Source/Angular/src/app/frontend/frontend.module.ts
+++ Source/Angular/src/app/frontend/frontend.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FrontendHomeComponent } from './home/component/frontend-home/frontend-home.component';
 import { FrontendRoutingModule } from './frontend-routing.module';
+import { FrontendHeaderComponent } from './layouts/frontend-header/frontend-header.component';
+import { FrontendFooterComponent } from './layouts/frontend-footer/frontend-footer.component';
 
 
 
 
 @NgModule({
   declarations: [
-    FrontendHomeComponent
+    FrontendHomeComponent,
+    FrontendHeaderComponent,
+    FrontendFooterComponent
   ],
   imports: [
     CommonModule,
