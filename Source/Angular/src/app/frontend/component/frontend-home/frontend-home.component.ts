76fcb0967cb7cb1841dfa620b0d64fee485efed9
Hoang Van Chung <hoang@hoang-HP-ProBook-640-G1chunghv.tny@gmail.com>
2021-07-02 13:44:14 +0700

    format code

--- Source/Angular/src/app/frontend/component/frontend-home/frontend-home.component.ts
+++ Source/Angular/src/app/frontend/component/frontend-home/frontend-home.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { User } from '../../models/user';
+
 @Component({
   selector: 'app-frontend-home',
   templateUrl: './frontend-home.component.html',
   styleUrls: ['./frontend-home.component.css'],
 })
 export class FrontendHomeComponent implements OnInit {
-  constructor(private userService: UserService, private router:Router) {}
+  // user = localStorage.getItem('user');
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
   logOut() {
     this.userService.logout().subscribe({
-      next:()=>{
+      next: () => {
+        this.toastr.success('Logout successful!');
         localStorage.clear();
         this.router.navigate(['']);
       },
-      error:()=>{
-
-      }
-    })
+      error: () => {},
+    });
   }
 }
