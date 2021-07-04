ab467fc46241dfc58c833ad58105679cdf378332
thanh <lanhthanh.dktd@gmail.com>
2021-07-04 15:00:02 +0700

    update routerlink

--- Source/Angular/src/app/backend/services/user.service.ts
+++ Source/Angular/src/app/backend/services/user.service.ts
@@ -12,18 +12,18 @@ export class UserService {
   constructor(private http: HttpClient, private route: Router) { }
 
   getAllProviders(): Observable<any> {
-    return this.http.get<any>(`${environment.base_Url}provider`)
+    return this.http.get<any>(`${environment.base_Url}admin/provider`)
   }
 
   getRequestingProvider(): Observable<any> {
-    return this.http.get<any>(`${environment.base_Url}provider/requesting`)
+    return this.http.get<any>(`${environment.base_Url}admin/provider/requesting`)
   }
 
   approveRequest(provider_id:any): Observable<any>{
-    return this.http.get<any>(`${environment.base_Url}provider/${provider_id}/approve`)
+    return this.http.get<any>(`${environment.base_Url}admin/provider/${provider_id}/approve`)
   }
 
   setVip(provider_id:any): Observable<any>{
-    return this.http.get<any>(`${environment.base_Url}provider/${provider_id}/setvip`)
+    return this.http.get<any>(`${environment.base_Url}admin/provider/${provider_id}/setvip`)
   }
 }
